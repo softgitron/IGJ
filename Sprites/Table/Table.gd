76bd0407d48575a85d339a18e3ce5d319f4b0c63
johan1954 <tomi.enberg@gmail.com>
2025-08-17 11:31:34 +0300

    Enum and table updated with new values regarding new items

--- Sprites/Table/Table.gd
+++ Sprites/Table/Table.gd
@@ -4,6 +4,11 @@ extends Area2D
 
 var shovel_texture = load("res://Items/Graphics/Shovel.png")
 var key_texture = load("res://Items/Graphics/ActivationKey.png")
+var keycard_texture = load("res://Items/Graphics/Keycard.png")
+var keycard_ultimate_texture = load("res://Items/Graphics/KeycardUltimate.png")
+var playstation_texture = load("res://Items/Graphics/PS4.png")
+var ps_controller_texture = load("res://Items/Graphics/PS4Controller.png")
+var semtex_texture = load("res://Items/Graphics/Semtex.png")
 
 var _item_name: Enums.ITEM_NAMES
 @export var item_name : Enums.ITEM_NAMES :
@@ -27,3 +32,14 @@ func update_item_texture():
 			$ItemDisplayer.set_rotation_degrees(90)
 		Enums.ITEM_NAMES.TELEPORTER_KEY:
 			$ItemDisplayer.texture = key_texture
+		Enums.PS_CONTROLLER:
+			$ItemDisplayer.texture = ps_controller_texture
+		Enums.KEYCARD:
+			$ItemDisplayer.texture = keycard_texture
+		Enums.KEYCARD_ULTIMATE:
+			$ItemDisplayer.texture = keycard_ultimate_texture
+		Enums.PLAYSTATION:
+			$ItemDisplayer.texture = playstation_texture
+		Enums.SEMTEX:
+			$ItemDisplayer.texture = semtex_texture
+		
